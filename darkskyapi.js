8851a2c5af7d958265cc3ccdf87812c83a2e474d
rskanb <rs.kanbi@gmail.com>
2018-12-02 13:03:06 -0800

    working on my local pc with chrome and safari browsers added one more functionality

--- darkskyapi.js
+++ darkskyapi.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         type: 'bar',
         // The data for our dataset
         data: {
-            labels: ["Your Current Location", "Your Home Location", "Your Dream Location", "Temp Difference from Home", "Temp Difference from Dream"],
+            labels: ["Current Location", "Home Location", "Dream Location", "Temp Difference from Home", "Temp Difference from Dream"],
             datasets: [{
                 label: "Temperatures",
                 backgroundColor: 'rgb(255, 99, 132)',
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 data: []
             }]
         }
-     //  Configuration options go here
+    //  Configuration options go here
     //  options: {
     //     animation: {
     //         onProgress: function(animation) {
@@ -24,56 +24,7 @@ $(document).ready(function(){
     //         }
     //     }
     });
-    
-    
-    // $("#add-dreamcity").addClass('hidden');
-    var homeCity;
-    var dreamCity
-    $("#add-homecity").on("click",function(event){
-        event.preventDefault();
-        $("#newdiv").empty();
-        homeCity = $("#home-city").val().trim();
-        var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.unsplash.com/search/photos?page=1&query=${homeCity}&client_id=90ca82a2be2184719136b24b6f8bf5978a1c75e992d067d7dacfb1aaf0c76675`
-        console.log(homeCity);
-        gethomecitypic(queryUrl);
-        homeCityWeather(homeCity);
-        $("#home-city").val('');
-    });
-    //Function to get Home City Picture
-    function gethomecitypic(queryUrl){
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).done(function(response){
-            console.log(response);
-            $("#homecityimage").attr('src', response.results[0].links.download);
-            console.log(response.results[0].links.download);
-        });
-    }
-    
-    $("#add-dreamcity").on("click", function(event){
-        event.preventDefault();
-        $("#newdiv1").empty();
-        var dreamCity = $("#dream-city").val().trim();
-        console.log(dreamCity);
-        var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.unsplash.com/search/photos?page=1&query=${dreamCity}&client_id=90ca82a2be2184719136b24b6f8bf5978a1c75e992d067d7dacfb1aaf0c76675`;
-        getdreamcitypic(queryUrl);
-        dreamCityWeather(dreamCity);
-        $("#dream-city").val('');
-    });
-    
-    //Function to get Dream Location Picture
-    function getdreamcitypic(queryUrl){
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).done(function(response){
-            console.log(response);
-            $("#dreamcityimage").attr('src', response.results[0].links.download);
-            //console.log(response.results[0].links.download);
-        });
-    }
-    
+
     //Function to get current latiture and longitude based on browser
     var geolocationCall = navigator.geolocation.getCurrentPosition(function(position) {
         if (navigator.geolocation){  //condition to check geolocation available
@@ -84,7 +35,6 @@ $(document).ready(function(){
             var text = document.createTextNode(`${response.city}, ${response.region}, ${response.country}`);
             paragraph.appendChild(text);
         }, "jsonp");
-        console.log(navigator.geolocation);
         getDarkWeather(lat, long)
         } else {
             var paragraph = document.getElementById("current-location");
@@ -93,7 +43,7 @@ $(document).ready(function(){
         }
       });
     
-    //Function to get provided latitue and longitude current weather
+    //Function to get provided latitue and longitude current weather based on browser
     function getDarkWeather(lat, long){
         //Example of query URL : https://api.darksky.net/forecast/[key]/[latitude],[longitude]
         var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/df67c7be35a4db36806ee00e0657e57d/${lat},${long}`
@@ -111,13 +61,129 @@ $(document).ready(function(){
             tempArray.push(currentTemp);
         });
     };
+
+    //remove data and attributes and make it as new page load only at home location div
+    function stopslide(){
+        $("#homecityimage").removeAttr();
+        $("image").removeAttr();
+        $("#homecityimage").attr('src','img.jpg');
+        $("#newdiv").empty();
+        $("#newdiv").removeData();
+    }
+    //remove data and attributes and make it as new page load only at dream location div
+    function stopdreamslide(){
+        $("#dreamcityimage").removeAttr();
+        $("image").removeAttr();
+        $("#dreamcityimage").attr('src','img.jpg');
+        $("#newdiv1").empty();
+        $("#newdiv1").removeData();
+        //$("#dream-location").removeData();
+    }
+    // $("#add-dreamcity").addClass('hidden'); for hiding button initially but not used
+    //Initially defininig the variables 
+    var homeCity;
+    var dreamCity
+    var t;
+    var d;
+
+    //Click event to add home location or home town/city
+    $("#add-homecity").on("click",function(event){
+        event.preventDefault();
+        stopslide();
+        homeCity = $("#home-city").val().trim();
+        var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.unsplash.com/search/photos?page=1&query=${homeCity}&client_id=90ca82a2be2184719136b24b6f8bf5978a1c75e992d067d7dacfb1aaf0c76675`;
+        gethomecitypic(queryUrl);
+        homeCityWeather(homeCity);
+        $("#home-city").val('');
+    });
+
+     //Click event to add dream location or dream town/city
+    $("#add-dreamcity").on("click", function(event){
+        event.preventDefault();
+        stopdreamslide();
+        dreamCity = $("#dream-city").val().trim();
+        var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.unsplash.com/search/photos?page=1&query=${dreamCity}&client_id=90ca82a2be2184719136b24b6f8bf5978a1c75e992d067d7dacfb1aaf0c76675`;
+        getdreamcitypic(queryUrl);
+        dreamCityWeather(dreamCity);
+        $("#dream-city").val('');
+    });
+
+    //Function to get Home City Picture and displaying on image source also changing image after every 5 second
+    function gethomecitypic(queryUrl){
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).done(function(response){
+        stop();
+        //blow commented out code runs till array length it does not repeate.
+        // $("#homecityimage").attr('src', response.results[i].links.download);   // original code
+        // function setSlideDelay(i) {
+        //     setTimeout(function(){
+        //      $("#homecityimage").attr('src', response.results[i].links.download);
+        //        console.log(response.results[i].links.download);
+        //      }, i*4000);
+        // };
+        // for (let i=0; i<=response.results.length; i++){
+        //     setSlideDelay(i)
+        //     console.log("for home city started");
+        //         //console.log(response.results[i].links.download);
+        //     //setTimeout(() => $("#homecityimage").attr('src', response.results[i].links.download), i*5000); 
+        // };
+        //below function executes continuously after reseting array length 
+        var i=0;
+        var homePicSlide = response.results.length;
+        function homeSlide(){
+            if(i >= homePicSlide){
+                i = 0;
+            }
+            $("#homecityimage").attr('src', response.results[i].links.download);
+            i++;       
+        }
+        function autohSlide(){
+            clearInterval(t);
+            t = setInterval(homeSlide, 5000)
+        }
+        function stop(){
+            clearInterval(t);
+        }
+        autohSlide();
+        });
+    }
+
+    //Function to get dream City Picture and displaying on image source also changing image after every 5 second
+    function getdreamcitypic(queryUrl){
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).done(function(response){
+        // $('#dreamcityimage').empty();
+        stopdream();
+        var i=0;
+        var dreamPicSlide = response.results.length;
+        function dreamSlide(){
+            if(i >= dreamPicSlide){
+                i = 0;
+            }
+            $("#dreamcityimage").attr('src', response.results[i].links.download);
+            i++;       
+        }
+        function autodSlided(){
+            clearInterval(d);
+            setInterval(dreamSlide, 5000)
+        }
+        function stopdream(){
+            clearInterval(d);
+        }
+        autodSlided();
+        });
+    }
+
+    //Function to get home location weather base on city input
     function homeCityWeather(place){
         $("#add-dreamcity").removeClass("hidden");
-      // debugger;
-    //Function to get current weather base on city and dream location
+        $("#newdiv").empty();
         var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=c63e722432e11165cac004ba48f2a376`;
         homeCityCall(queryUrl);
-        
         //$("#add-dreamcity").css('text-align','center');
     }
     function homeCityCall(url){
@@ -125,14 +191,13 @@ $(document).ready(function(){
             url: url,
             method: "GET"
         }).done(function(response){
-            console.log(response);
+            stopslide();
             var cityTemperature = parseInt(((response.main.temp-273.15)*1.8)+32);
             var humid = response.main.humidity;
             var windsp = response.wind.speed;
-            var sunRise = (response.sys.sunrise)*1000;
-            console.log(sunRise);
-            var day = moment(sunRise).format("DD-MM-YYYY HH:mm:ss A");
-            console.log(day);
+            //Not used sunrise and sunset moment js as api returns base on current browser location time which is not useful
+            // var sunRise = (response.sys.sunrise)*1000;
+            // var day = moment(sunRise).format("DD-MM-YYYY HH:mm:ss A");
             var newDiv = $("<div>");
             var cityTemp = $("<p>").text(`Temperature: ${cityTemperature} ℉`);
             var currCity = $("<p>").text(`${response.name}, ${response.sys.country}`);
@@ -149,8 +214,9 @@ $(document).ready(function(){
         });
     };
     
-    //Function to call dream city openweather api
+     //Function to get dream city/town/location weather base on input
     function dreamCityWeather(dreamplace){
+        $("#newdiv1").empty();
         var queryUrl = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?q=${dreamplace}&APPID=c63e722432e11165cac004ba48f2a376`;
         dreamCityCall(queryUrl);
     }
@@ -161,7 +227,7 @@ $(document).ready(function(){
             url: url,
             method: "GET"
         }).done(function(response){
-            console.log(response);
+            stopdreamslide();
             var dreamCityTemp = parseInt(((response.main.temp-273.15)*1.8)+32);
             var dhumid = response.main.humidity;
             var dwindsp = response.wind.speed;
@@ -170,7 +236,7 @@ $(document).ready(function(){
             var dreamCity = $("<p>").text(`${response.name}, ${response.sys.country}`);
             var dcityWind = $("<p>").text(`Wind Speed: ${dwindsp}`);
             var dcityHumid = $("<p>").text(`Humidity: ${dhumid}`);
-            //Latitude and longitude extracted to use for getDarkWeather API in case needed
+            //Latitude and longitude extracted to use for getDarkWeather API in case needed for future development
             //var lat = response.coord.lat;
             //var long = response.coord.lon;
             newDiv1.attr("id", "newdiv1");
@@ -186,7 +252,6 @@ $(document).ready(function(){
     };
     //Building chart based on temperature of all cities/location
     function buildChart(temp, chart){
-        console.log("hello")
         // chart.data.datasets.forEach((dataset) => {
         //     dataset.data.push(data);
         // });
